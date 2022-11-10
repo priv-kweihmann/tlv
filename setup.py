a8624a45adab8e93868096dc2326678228fcd223
priv-kweihmann <priv-kweihmann@localhost>
2022-11-10 20:34:36 +0000

    Automatically updated using GitHub Actions

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="tlv",
-    version="1.3.1",
+    version="1.3.2",
     author="Konrad Weihmann",
     author_email="kweihmann@outlook.com",
     description="-- Too less variation -- Find duplicates in source code for various languages",
